676f346447d038bd6d0129560cf50b706af8d426
Dimitrios C. Michalakos <dimitris@jmike.gr>
2012-10-10 14:09:42 +0300

    A bit of refactoring.

--- source/typewriter.js
+++ source/typewriter.js
@@ -1,98 +1,103 @@
-/**
- * A jquery text animation effect that imitates typewriter machines.
- */
 (function ($) {
 
-    $.fn.typewrite = function (settings, callback) {
+    /**
+     * Travels down the DOM tree of the designated element and returns an array of text nodes.
+     * @param el {Object} a DOM element.
+     * @return {Array.<Object>}.
+     */
+    function getTextNodes(el) {
+        var textNodes = [];
+        function traverse(node) {
+            var children, i;
+            if (node.nodeType === 3 && !/^\s*$/.test(node.nodeValue)) {//text node non empty
+                textNodes.push(node);
+            } else {
+                children = node.childNodes;
+                for (i = 0; i < children.length; i++) {
+                    traverse(children[i]);
+                }
+            }
+        }
+        traverse(el);
+        return textNodes;
+    }
 
+    /**
+     * Progressively types the given string in the specified node.
+     * @param node {Object} a DOM TextNode.
+     * @param str {string} the string to type.
+     * @param delay {number} time delay between character typings.
+     * @param i {number} position of character currently typing.
+     * @param callback {function=} (optional).
+     */
+    function type(node, str, delay, i, callback) {
+        if (i < str.length) {
+            node.nodeValue = str.substr(0, ++i);
+            setTimeout(function () {
+                type(node, str, delay, i, callback);
+            }, delay);
+        } else if (typeof callback === "function") {
+            callback.call(node);
+        }
+    }
+
+    /**
+     * Blinks the cursor at the end of a string.
+     * @param node {Object} a DOM node holding the cursor.
+     * @param delay {number} time delay between cursor blinks.
+     * @return {number} the id of the blinking interval.
+     */
+    function blink(node, delay) {
+        var hidden = true;
+        return setInterval(function () {
+            hidden = !hidden;
+        }, delay);
+    }
+
+    /**
+     * Performs a jQuery text animation effect that imitates typewriter machines.
+     * @param settings {Object=}.
+     * @param callback {function=}.
+     */
+    $.fn.typewrite = function (settings, callback) {
+        var textNodes, i, node;
+        //make sure settings are specified
         settings = $.extend({
-            speed: 60,
+            type: 60,
             blink: 200,
             cursor: "_"
         }, settings);
-
-        /**
-         * Performs a deep scan and returns an array of text nodes found under the designated element.
-         * @param el {Object} a DOM element.
-         * @return {Array.<Object>}.
-         */
-        function getTextNodes(el) {
-            var textNodes = [];
-            function traverse(node) {
-                var children, i;
-                if (node.nodeType === 3 && !/^\s*$/.test(node.nodeValue)) {
-                    textNodes.push(node);
-                } else {
-                    children = node.childNodes;
-                    for (i = 0; i < children.length; i++) {
-                        traverse(children[i]);
-                    }
-                }
-            }
-            traverse(el);
-            return textNodes;
-        }
-
-        /**
-         * Blinks the cursor at the end of the string.
-         * @param node {Object} the cursor node.
-         * @return {number} the id of the blinking interval.
-         */
-        function blink(node) {
-            var hidden = true;
-            return setInterval(function() {
-                node.nodeValue = hidden ? " " : settings.cursor;
-                hidden = !hidden;
-                console.log(hidden, node.nodeValue);
-            }, settings.blink);
-        }
-
-        /**
-         * Typewrites the given string in the specified text node.
-         * @param node {Object} a DOM TextNode.
-         * @param str {string} the string to type.
-         * @param i {number=} position of character currently typing (optional).
-         * @param callback {function=} (optional).
-         */
-        function type(node, str, i, callback) {
-            i = i || 0;
-            if (i < str.length) {
-                node.nodeValue = str.substr(0, ++i);
-                setTimeout(function () {
-                    type(node, str, i, callback);
-                }, settings.speed);
-            } else if (typeof callback === "function") {
-                callback.call(node);
-            }
-        }
-
+        //retrieve all text nodes
+        textNodes = [];
         this.each(function () {
-            var textNodes, i, node;
-            textNodes = getTextNodes(this);
-            for (i = 0; i < textNodes.length; i++) {
-                node = textNodes[i];
-                node._data = node.nodeValue;
-                node.nodeValue = "";
-            }
-            function traverse(textNodes, i) {
-                var node, blinking;
-                i = i || 0;
-                if (i < textNodes.length) {
-                    node = textNodes[i];
-                    cursor = document.createTextNode(settings.cursor);
-                    $(node).after(cursor);
-                    blinking = blink(cursor);
-                    console.log(i, blinking, node, cursor);
-                    type(node, node._data, 0, function () {
-                        clearInterval(blinking);
-                        cursor.nodeValue = " ";
-                        delete node._data;//delete extra data
-                        traverse(textNodes, i + 1);
-                    });
-                }
-            }
-            traverse(textNodes);
+            textNodes = textNodes.concat(getTextNodes(this));
         });
-
+        //empty the text nodes, store value in hidden property
+        for (i = 0; i < textNodes.length; i++) {
+            node = textNodes[i];
+            node._data = node.nodeValue;
+            node.nodeValue = "";
+        }
+        //create function to traverse the text nodes
+        function traverse(textNodes, i) {
+            var node, cursor, blinking;
+            i = i || 0;
+            if (i < textNodes.length) {
+                node = textNodes[i];
+                cursor = document.createTextNode(settings.cursor);
+                $(node).after(cursor);
+                blinking = blink(cursor, settings.blink);
+                type(node, node._data, settings.type, 0, function () {
+                    clearInterval(blinking);
+                    cursor.nodeValue = " ";
+                    delete node._data;//delete extra data
+                    traverse(textNodes, i + 1);
+                });
+            } else if (typeof callback === "function") {
+                callback.call(null);
+            }
+        }
+        //traverse the text nodes
+        traverse(textNodes);
     };
 }(jQuery));
